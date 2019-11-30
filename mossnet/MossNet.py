8ab5b4a4622dce459a3d0e7068eb5a0929be3a43
Niema Moshiri <niemamoshiri@gmail.com>
2019-11-30 12:35:53 -0800

    Added save method

--- mossnet/MossNet.py
+++ mossnet/MossNet.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 from gzip import open as gopen
 from networkx import MultiDiGraph
-from pickle import load
+from pickle import dump,load
 
 class MossNet:
     def __init__(self, moss_results_dict):
-        '''Create a ``MossNet`` object from a 2D dictionary of downloaded MOSS results
+        '''Create a ``MossNet`` object from a 3D dictionary of downloaded MOSS results
 
         Args:
-            ``moss_results_dict`` (``dict``): A 2D dictionary of downloaded MOSS results
+            ``moss_results_dict`` (``dict``): A 3D dictionary of downloaded MOSS results
 
         Returns:
             ``MossNet``: A ``MossNet`` object
@@ -38,3 +38,22 @@ class MossNet:
                     except:
                         raise TypeError("moss_results_dict must be a 3D dictionary of MOSS results")
                     self.graph.add_edge(u, v, attr_dict = {'file':f, 'percent':percent, 'html1':html1, 'html2':html2})
+
+    def save(self, outfile):
+        '''Save this ``MossNet`` object as a 3D dictionary of MOSS results
+
+        Args:
+            ``outfile`` (``str``): The desired output file's path
+        '''
+        out = dict()
+        for u in self.graph.nodes:
+            u_edges = dict(); out[u] = u_edges
+            for v in self.graph.neighbors(u):
+                u_v_links = dict(); u_edges[v] = u_v_links; u_v_edge_data = self.graph.get_edge_data(u,v)
+                for k in u_v_edge_data:
+                    edge = u_v_edge_data[k]['attr_dict']; u_v_links[edge['file']] = (edge['percent'], edge['html1'], edge['html2'])
+        if outfile.lower().endswith('.gz'):
+            f = gopen(outfile, mode='wb', compresslevel=9)
+        else:
+            f = open(outfile, 'wb')
+        dump(out, f); f.close()
